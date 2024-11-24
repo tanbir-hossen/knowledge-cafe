9b3bedad601daf699ea2c8326750177c73994186
tanbir-hossen <tanbir8hossen@gmail.com>
2024-11-24 18:29:59 +0600

    created fake json blog data

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h3 className="text-4xl font-bold">
+                Knowledge Cafe
+            </h3>
+        </div>
+    );
+};
+
+export default Header;
